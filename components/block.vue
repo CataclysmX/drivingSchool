55be7f9546b88bd2cf88c2fb22914c37d055a3e6
CataclysmX <mamath42240@gmail.com>
2022-03-12 15:23:12 +0100

    add Watcher

---
+++ components/block.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="w-full bg-blue-500">
+    <div class="container mx-auto">
+      <span>dzqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
